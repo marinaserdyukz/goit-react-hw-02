af3d568182a69cc456b06a1cad0a84cfb40fe0c6
Marina Serdiuk <serdyukmarina876@gmail.com>
2025-03-04 20:25:08 +0100

    created components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import css from "./App.module.css"
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
