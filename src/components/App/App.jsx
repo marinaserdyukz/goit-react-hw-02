7d0ec51c4f50d1d3b13413490ca5a5ef68f2baf3
Marina Serdiuk <serdyukmarina876@gmail.com>
2025-03-06 22:54:41 +0100

    steps 1-6

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
-import css from "./App.module.css"
+import { useState, useEffect } from 'react'
+import Description from '../Description/Description'
+import Feedback from '../Feedback/Feedback'
+import Options from "../Options/Options"
+import Notification from "../Notification/Notification"
 
+  
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
+  });
+
+    useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+   ...prevFeedback,
+          [feedbackType]: prevFeedback[feedbackType] + 1}
+    ))
+  }
+  
+const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100)
+
+
 
   return (
     <>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback} />
+      {totalFeedback > 0 ? <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} /> : <Notification/>}
+      
       
     </>
   )
 }
+
 
